20351b51a193c2b2306d97112fc5a503d7085850
Kerry Buckley <kerryjbuckley@gmail.com>
2010-05-14 10:45:36 +0100

    Add authentication failure to demo

--- demo.rb
+++ demo.rb
@@ -3,32 +3,28 @@
 require 'rubygems'
 require 'oauth'
 
-consumer = OAuth::Consumer.new "key1", "secret1",
+options = {
   :site               => "http://localhost:9292",
   :scheme             => :header,
-  :http_method        => :post,
-  :proxy => ENV["http_proxy"]
+  :http_method        => :post
+}
 
-access_token = OAuth::AccessToken.new consumer
 puts "key1:"
+consumer = OAuth::Consumer.new "key1", "secret1", options
+access_token = OAuth::AccessToken.new consumer
 puts access_token.get "/"
 
-consumer = OAuth::Consumer.new "key2", "secret2",
-  :site               => "http://localhost:9292",
-  :scheme             => :header,
-  :http_method        => :post,
-  :proxy => ENV["http_proxy"]
-
+puts "\nkey1 with wrong secret:"
+consumer = OAuth::Consumer.new "key1", "wrong_secret", options
 access_token = OAuth::AccessToken.new consumer
+puts access_token.get "/"
+
 puts "\nkey2:"
+consumer = OAuth::Consumer.new "key2", "secret2", options
+access_token = OAuth::AccessToken.new consumer
 puts access_token.get "/content_cache", "Accept" => "application/xml"
 
-consumer = OAuth::Consumer.new "key3", "secret3",
-  :site               => "http://localhost:9292",
-  :scheme             => :header,
-  :http_method        => :post,
-  :proxy => ENV["http_proxy"]
-
-access_token = OAuth::AccessToken.new consumer
 puts "\nkey3:"
+consumer = OAuth::Consumer.new "key3", "secret3", options
+access_token = OAuth::AccessToken.new consumer
 puts access_token.get "/content_cache", "Accept" => "application/xml"
